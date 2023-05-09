08642e9467eb89394a46d723d3f1f2a6f97958f8
ShahFH <shahfaisal.swe@example.com>
2023-05-09 22:12:11 +0430

    homepage done, now working on articles page

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 function Articles() {
+  const posts = [/* Array of posts fetched from GraphQL */];
+
   return (
     <section>
-      {/* Articles section content */}
+      <h2>All Articles</h2>
+      <div className="posts">
+        {posts.map((post) => (
+          <div key={post.id} className="post">
+            <a href={`/post/${post.id}`}>
+              <img src={post.image} alt={post.title} />
+              <h3>{post.title}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
+      {posts.length > 4 && (
+        <div className="pagination">
+          {/* Pagination buttons here */}
+        </div>
+      )}
     </section>
   );
 }
 
 export default Articles;
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../style.css';
+import bgImg from '../bgImgsvg.svg';
+import Image from '../Image.jpg';
 
 function Home() {
   return (
     <section className='Home'>
       <div className='hero'>
-        <h1 className='hero-title'>Welcome to My Website</h1>
+      <img className="bgImg" src={bgImg} alt="Background-Img" />
+        <h1 className='hero-title'>CodeBloge</h1>
         <p className='hero-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <img className='hero-image' src='your-image-url.jpg' alt='Hero Image' />
+        <img className='hero-image' src={Image} alt='Hero Image' />
       </div>
     </section>
   );
