eaed7ee6eabdaa3fbea1b83fb46444eb42bfc5a4
ShahFH <shahfaisal.swe@example.com>
2023-05-28 13:24:52 +0430

    Updated home page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,16 +6,19 @@ import Image from '../Image.jpg';
 function Home() {
   return (
     <section className='Home'>
-  <div className='hero container relative mx-auto my-0 min-h-full max-w-4xl md:px-5 mb-4'>
+  <div className='hero container relative mx-auto my-0 min-h-full max-w-4xl md:px-5 mb-4 px-14'>
     {/*<img className="bgImg" src={bgImg} alt="Background-Img" />*/}
-    <div className=''>
-      <h1 className="text-white pt-10 text-8xl text-left font-bold leading-tight tracking-tight">
-        Personal Blog
+
+    <div className='px-4 mx-auto max-w-8xl lg:px-4 lg:text-center'>
+      <h1 className="text-white mt-8 text-4xl font-bold lg:font-extrabold lg:text-6xl lg:leading-none dark:text-white lg:text-center xl:px-48 lg:mb-7">
+        Welcome to CoBloge
       </h1>
-      <p className="font-normal text-xl font-[SF Mono] text-white leading-[1.7] h-auto">
-        Why Ghost (& Figma) instead of Medium, WordPress or other options?
+      <p className=" text-white mt-10 mb-4 text-lg font-normal lg:text-center lg:text-xl xl:px-60">
+      Join me on CoBloge,👋 where I share my personal experiences, insights, and adventures as a software developer.
+      Let's embark on this journey together and elevate our skills in the ever-evolving realm of technology.
       </p>
     </div>
+
     <div className='mt-8'>
       <img className='hero-image' src={Image} alt='Hero Image' />
     </div>
